7eed0c0a1a271d99f540cb89c5ee427c93193642
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-08-18 22:24:33 +0200

    Create 01_read_spreadsheet.py

---
+++ 01_read_spreadsheet.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 01_read_spreadsheet.py
+
+# Dependencies
+import os
+import pandas
+
+path = 'C:\\Users\\user\\spreadsheets' # Spreadsheets are stored within this directory
+os.chdir(path)
+
+for dirname, subfolders, filenames in os.walk(path):
+    for filename in filenames:
+        # Find and read a single spreadsheet into DataFrame:
+        if filename.find('xls') != -1:
+            print(f'READING:\t{filename}')
+            df = pd.read_excel(filename, sheet_name="Sheet1", skiprows=0, usecols="A:F")
+
+# Show DataFrame object info:
+df.info()
