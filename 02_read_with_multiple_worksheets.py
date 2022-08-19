8e572cbaee5118b665b19bed466b69761cdf7915
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-08-19 22:34:40 +0200

    Create 02_read_with_multiple_worksheets.py

---
+++ 02_read_with_multiple_worksheets.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# Python 3.9.5
+
+# 01_read_spreadsheet.py
+
+# Dependencies
+import os
+import pandas
+
+path = 'C:\\Users\\user\\spreadsheets' # Spreadsheets are stored within this directory
+os.chdir(path)
+
+# ============================================================================================
+# Access WorkBook, make multiple DataFrame objects by using the with-method:
+for dirname, subfolders, filenames in os.walk(path):
+    for filename in filenames:
+        if filename.find('xls') != -1:
+            with pd.ExcelFile("workbook.xlsx") as f:
+                df1 = pd.read_excel(f, "Sheet1", skiprows=1, usecols="B:F", nrows=2)
+                df2 = pd.read_excel(f, "Sheet2", skiprows=1, usecols="B:F", nrows=2)
+
+print(df1)
+print(df2)
