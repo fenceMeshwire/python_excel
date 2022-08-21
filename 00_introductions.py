0caf18950777867c741c3cba2852a6ef7b8b7ed9
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-08-21 14:22:58 +0200

    Update 00_introductions.py

--- 00_introductions.py
+++ 00_introductions.py
@@ -19,19 +19,21 @@ file_formats = {'xlsx': xlsx, 'xlsm': xlsm, 'xltx': xltx, 'xltm': xltm, 'xlsb': 
 
 try:
     while True:
+        print()
         print('Please choose the following options (q for exit):')
-        print('======================================')
-        print('xlsx, xlsm, xltx, xltm, xlsb, xls, xlt')
-        print('======================================')
-        file_format_input = input('Please enter the Excel File Format:')
+        print('OPTIONS:', 'xlsx, xlsm, xltx, xltm, xlsb, xls, xlt')
+        print()
+        file_format_input = input('Please enter the Excel File Format: >>> ')
         file_format_input = file_format_input.lower()
         for file_format in file_formats:
             if file_format_input == file_format:
                 print()
+                print('Modules to use:')
+                print('=' * 40)
                 print('Read:\t', file_formats[file_format]["read"])
                 print('Write:\t', file_formats[file_format]["write"])
                 print('Edit:\t', file_formats[file_format]["edit"])
-                print()
+                print('=' * 40)
             if file_format_input == 'q':
                 print('End of program.')
                 sys.exit()
