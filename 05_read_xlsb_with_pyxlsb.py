7f9d844d58959b83fa698657922986c2d0a5417c
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-08-22 23:23:00 +0200

    Update 05_read_xlsb_with_pyxlsb.py

--- 05_read_xlsb_with_pyxlsb.py
+++ 05_read_xlsb_with_pyxlsb.py
@@ -10,29 +10,32 @@ import os
 import pandas as pd
 import pyxlsb
 
-# Change the current working directory:
-path = 'C:\\Users\\user\\...'
-os.chdir(path)
-
-# Walk through the current working directory:
-for dirname, subfolders, filenames in os.walk(path):
-    # Check each filename for xlsb:
-    for filename in filenames:
-        if filename.find('xlsb') != -1:
-            result = filename
-            with pyxlsb.open_workbook(filename) as workbook:
-                for worksheet in (workbook.sheets):
-                    print("WorkSheet.Name =", worksheet)
-                    with workbook.get_sheet(worksheet) as WorkSheet:
-                        worksheet_usedrange = WorkSheet.dimension
-                        print("Worksheet.UsedRange =", worksheet_usedrange)
-                        print()
+def get_xlsb_workbooks(path):
+    result = []
+    # Walk through the current working directory:
+    for dirname, subfolders, filenames in os.walk(path):
+        # Check each filename for xlsb:
+        for filename in filenames:
+            if filename.find('xlsb') != -1:
+                result.append(filename)
+                with pyxlsb.open_workbook(filename) as workbook:
+                    for worksheet in (workbook.sheets):
+                        print("WorkSheet.Name =", worksheet)
+                        with workbook.get_sheet(worksheet) as WorkSheet:
+                            worksheet_usedrange = WorkSheet.dimension
+                            print("Worksheet.UsedRange =", worksheet_usedrange)
+                            print()
+    return result
                         
-# Import each WorkSheet into a DataFrame object.
-# Count number of WorkSheets previously.
-with pyxlsb.open_workbook(result) as workbook:
-    df = pd.read_excel(workbook, 0, engine='pyxlsb')
+if __name__ == '__main__':
+    # Change the current working directory:
+    path = 'C:\\Users\\user\\...}'
+    os.chdir(path)
+    # Obtain a list of all xlsb workbooks
+    workbooks = get_xlsb_workbooks(path)
+    # Open the first WorkSheet of the first WorkBook
+    with pyxlsb.open_workbook(workbooks[0]) as workbook:
+        df = pd.read_excel(workbook, 0, engine='pyxlsb')
 
-# Check the imported data:
-df.head()
-              
+    # Check the imported data:
+    df.head()
